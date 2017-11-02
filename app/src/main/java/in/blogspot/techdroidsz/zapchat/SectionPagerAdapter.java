e27c2655ec44ebe882c0d33cb092f7a0f2d8034b
Geeky4 <abhishek46810@gmail.com>
2017-11-02 18:22:49 +0530

    Initial commit

---
+++ app/src/main/java/in/blogspot/techdroidsz/zapchat/SectionPagerAdapter.java
@@ -0,0 +1,24 @@
+package in.blogspot.techdroidsz.zapchat;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Abhishek on 02-11-2017.
+ */
+
+class SectionsPagerAdapter  extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position){
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
