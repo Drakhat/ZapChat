96413574de872e8c9d8b3903e50fbd8ad6a57437
Geeky4 <abhishek46810@gmail.com>
2017-11-04 16:13:57 +0530

    All User Activity Created with Profile Photo and Status getting Updated. (Thumbnail Included in All users too). Users Profile added.

--- app/src/main/java/in/blogspot/techdroidsz/zapchat/ProfileActivity.java
+++ app/src/main/java/in/blogspot/techdroidsz/zapchat/ProfileActivity.java
@@ -1,5 +1,6 @@
 package in.blogspot.techdroidsz.zapchat;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -23,6 +25,9 @@ public class ProfileActivity extends AppCompatActivity {
 
 
     private DatabaseReference mUsersDatabase;
+
+    private ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +44,12 @@ public class ProfileActivity extends AppCompatActivity {
         mProfileFriendsCount=(TextView) findViewById(R.id.profile_totalFriends);
         mProfileSendReqBtn=(Button) findViewById(R.id.profile_send_req_btn);
 
+        mProgressDialog=new ProgressDialog(this);
+        mProgressDialog.setTitle("Loading User Data");
+        mProgressDialog.setMessage("Please wait while we load the user data.");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
 
         mUsersDatabase.addValueEventListener(new ValueEventListener() {
             @Override
@@ -50,6 +61,11 @@ public class ProfileActivity extends AppCompatActivity {
 
                 mProfileName.setText(display_name);
                 mProfileStatus.setText(status);
+
+
+                Picasso.with(ProfileActivity.this).load(image).placeholder(R.drawable.default_dp).into(mProfileImage);
+
+                mProgressDialog.dismiss();
             }
 
             @Override
